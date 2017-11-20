6d0153f07c1f6ff4f7ecac59148f1ed08bf6505d
Mossi HASHEMI <mossi.hashemi@gmail.Com>
2017-11-20 19:01:06 +0100

    make VM

---
+++ mk_vm.lisp
@@ -0,0 +1,17 @@
+;fonction de création de la vm
+;nomVm: nom de la vm sur laquelle faire l'opération
+;tailleMem: la taille de la memoire allouer a la vm
+(defun make-vm (nomVm tailleMem)
+  (progn
+
+      ;création des registres spéciaux
+
+      ;création des registres de données
+      
+      ;création des drapeaux
+
+    nomVm))
+
+
+
+

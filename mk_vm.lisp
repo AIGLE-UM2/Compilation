0f37eddfaef2dabffc1d202d981c4aeb8999bdd6
Mossi HASHEMI <mossi.hashemi@gmail.Com>
2017-11-20 22:18:28 +0100

    Ajouter le Mémoir à VM

--- mk_vm.lisp
+++ mk_vm.lisp
@@ -5,12 +5,26 @@
   (progn
 
       ;création des registres spéciaux
+      (setf (get nomVm 'FP) 0) ;Frame Pointer
+      (setf (get nomVm 'SP) 0) ;Stack Pointer
+      (setf (get nomVm 'PC) 0) ;Program Counter
 
       ;création des registres de données
-      
+      (setf (get nomVm 'R0) 0)
+      (setf (get nomVm 'R1) 0)
+      (setf (get nomVm 'R2) 0)
+
+
       ;création des drapeaux
 
+      ;création de la memoire
+      (setf (get nomVm 'memoire) (make-array tailleMem :initial-element ())) ;https://www.tutorialspoint.com/lisp/lisp_arrays.htm
+
+
+
     nomVm))
+
+
 
 
 

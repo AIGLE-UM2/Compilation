76107e0e1c0ad5da3fc43be76c18ac5c806a77c9
Mossi HASHEMI <mossi.hashemi@gmail.Com>
2017-11-21 17:59:30 +0100

    2_1: Fonction *move* de registre/valeur immédiate à registre

--- mk_vm.lisp
+++ mk_vm.lisp
@@ -14,66 +14,84 @@
 
 
 ;-----------------------------------------------------------------------------------------------------------;
-;       1_1: make-vm : création une machine virtuelle, avec un nom, une certaine taille de mémoire, etc.    ;
+;     1_1: *make-vm* : création une machine virtuelle, avec un nom, une certaine taille de mémoire, etc.    ;
 ;-----------------------------------------------------------------------------------------------------------;
-;nomVm: nom de la vm sur laquelle faire l'opération
+;nomVM: nom de la vm sur laquelle faire l'opération
 ;tailleMem: la taille de la memoire allouer a la vm
-(defun make-vm (nomVm tailleMem)
+(defun make-vm (nomVM tailleMem)
   (progn
 
       ;création des registres spéciaux
-      (setf (get nomVm 'FP) 0) ;Frame Pointer
-      (setf (get nomVm 'SP) 0) ;Stack Pointer
-      (setf (get nomVm 'PC) 0) ;Program Counter
+      (setf (get nomVM 'FP) 0) ;Frame Pointer
+      (setf (get nomVM 'SP) 0) ;Stack Pointer
+      (setf (get nomVM 'PC) 0) ;Program Counter
 
       ;création des registres de données
-      (setf (get nomVm 'R0) 0)
-      (setf (get nomVm 'R1) 0)
-      (setf (get nomVm 'R2) 0)
+      (setf (get nomVM 'R0) 0)
+      (setf (get nomVM 'R1) 0)
+      (setf (get nomVM 'R2) 0)
 
 
       ;création des drapeaux
 
       ;création de la memoire
-      (setf (get nomVm 'memoire) (make-array tailleMem :initial-element ()))   
+      (setf (get nomVM 'memoire) (make-array tailleMem :initial-element ()))   
 
-    nomVm))
+    nomVM))
 
 
 ;-----------------------------------------------------------------------------------------------------------;
-;           1_2:  Fonctions get-registe/set-register pour accéder au contenu des registres                  ;  
+;         1_2:  Fonctions *get-registe/set-register* pour accéder au contenu des registres                  ;  
 ;-----------------------------------------------------------------------------------------------------------;
 ;fonction d'acces en lecture aux registres
 ;reg: nom du registre a lire
-(defun get-register (nomVm reg)
-  (get nomVm reg))
+(defun get-register (nomVM reg)
+  (get nomVM reg))
 
 ;fonction d'acces en ecritureaux registres
 ;new: nouvelle valeur à affecter au registre
-(defun set-register (nomVm reg new)
-  (setf(get nomVm reg) new))
+(defun set-register (nomVM reg new)
+  (setf(get nomVM reg) new))
 
 
 ;-----------------------------------------------------------------------------------------------------------;
-;         1_3: Fonctions get-memory/set-memory pour accéder au contenu de la mémoire à une adresse donnée.  ;              ;
+;       1_3: Fonctions *get-memory/set-memory* pour accéder au contenu de la mémoire à une adresse donnée.  ;              
 ;-----------------------------------------------------------------------------------------------------------;
 
 ;fonction d'acces en lecture à la memoire
 ;pos: position de l'information à lire dans la mémoire
-(defun get-memory (nomVm pos)
-  (aref (get nomVm 'memoire) pos))  
+(defun get-memory (nomVM pos)
+  (aref (get nomVM 'memoire) pos))  
   
 
 ;fonction d'acces en ecriture à la memoire
 ;new: nouvelle valeur à affecter au registre
-(defun set-memory (nomVm pos new)
-  (setf(aref (get nomVm 'memoire) pos) new))
+(defun set-memory (nomVM pos new)
+  (setf(aref (get nomVM 'memoire) pos) new))
 
+
+
+;-----------------------------------------------------------------------------------------------------------;
+;                     2_1: Fonction *move* de registre/valeur immédiate à registre                          ;
+;-----------------------------------------------------------------------------------------------------------;
+
+(defun registerp (expression)
+  (member expression '(R0 R1 R2 FP SP PC)))
+
+;fonction de copie de R0 dans R1
+;R0: donnée à copier, peut être soit un registre soit une valeur
+;R1: registre de reception de copie
+(defun move(nomVM R0 R1)
+  (if(registerp R1)
+      (if(registerp R0)
+    (set-register nomVM R1 (get-register nomVM R0))
+    (set-register nomVM R1 r0))
+    (print '"ERREUR: Le recepteur de la copie (MOVE) n'est pas un registre")))
 
 
 ;___________________________________________________________________________________________________________;
 ;                                        * Bibliographie *                                                  ;
 ;                                                                                                           ;         
 ;     ▪ https://www.tutorialspoint.com/lisp/lisp_symbols.htm                                                ;
-;     ▪ https://www.tutorialspoint.com/lisp/lisp_arrays.htm                                                                                                   ;
+;     ▪ https://www.tutorialspoint.com/lisp/lisp_arrays.htm                                                 ;
 ;___________________________________________________________________________________________________________;

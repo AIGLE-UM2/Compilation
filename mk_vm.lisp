d46c3bc66adc952b21ad2ec9cde6c363c6f7a89f
Mossi HASHEMI <mossi.hashemi@gmail.Com>
2017-11-21 16:19:14 +0100

    1-STRUCTURES DE DONNÉES == Done

--- mk_vm.lisp
+++ mk_vm.lisp
@@ -1,4 +1,21 @@
-;fonction de création de la vm
+;▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓;
+;                                     ►►   Implémentation de la VM  ◄◄                                    ;
+;                                                                                                         ;
+;                                          ☻ Réalisé par :                                                ;
+;                                              Mostafa HASHEMI                                            ;
+;                                              Rémi DE WISPELAERE                                         ;
+;                                              François JOURDAIN                                          ;
+;                                                                                                         ;
+;                                          ● Sous la direction de :                                       ;
+;                                               Monsieur David Delahaye                                   ;
+;                                               Monsieur Mathieu Lafourcade                               ;
+;                                                                                                         ;
+;▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓▒░▓;
+
+
+;-----------------------------------------------------------------------------------------------------------;
+;       1_1: make-vm : création une machine virtuelle, avec un nom, une certaine taille de mémoire, etc.    ;
+;-----------------------------------------------------------------------------------------------------------;
 ;nomVm: nom de la vm sur laquelle faire l'opération
 ;tailleMem: la taille de la memoire allouer a la vm
 (defun make-vm (nomVm tailleMem)
@@ -18,14 +35,50 @@
       ;création des drapeaux
 
       ;création de la memoire
-      (setf (get nomVm 'memoire) (make-array tailleMem :initial-element ())) ;https://www.tutorialspoint.com/lisp/lisp_arrays.htm
-
-
+      (setf (get nomVm 'memoire) (make-array tailleMem :initial-element ()))   ;https://www.tutorialspoint.com/lisp/lisp_arrays.htm
 
     nomVm))
 
 
+;-----------------------------------------------------------------------------------------------------------;
+;           1_2:  Fonctions get-registe/set-register pour accéder au contenu des registres                  ;  
+;-----------------------------------------------------------------------------------------------------------;
+;fonction d'acces en lecture aux registres
+;reg: nom du registre a lire
+(defun get-register (nomVm reg)
+  (get nomVm reg))
+
+;fonction d'acces en ecritureaux registres
+;new: nouvelle valeur à affecter au registre
+(defun set-register (nomVm reg new)
+  (setf(get nomVm reg) new))
+
+
+;-----------------------------------------------------------------------------------------------------------;
+;         1_3: Fonctions get-memory/set-memory pour accéder au contenu de la mémoire à une adresse donnée.  ;              ;
+;-----------------------------------------------------------------------------------------------------------;
+
+;fonction d'acces en lecture à la memoire
+;pos: position de l'information à lire dans la mémoire
+(defun get-memory (nomVm pos)
+  (aref (get nomVm 'memoire) pos))  
+  
+
+;fonction d'acces en ecriture à la memoire
+;new: nouvelle valeur à affecter au registre
+(defun set-memory (nomVm pos new)
+  (setf(aref (get nomVm 'memoire) pos) new))
 
 
 
+
+
+
+
+;▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒;
+;                                        ♣ Bibliographie ♣                                                  ;
+;                                                                                                           ;         
+;     ▪ https://www.tutorialspoint.com/lisp/lisp_symbols.htm                                                ;
+;     ▪                                                                                                     ;
+;▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒░▒;
 
